637744a661773016e215326b4d2666dc22e38f81
Revati4 <o.a.bat@yandex.ru>
2023-01-22 10:14:38 +0300

    Initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String name;
+        double price;
+        System.out.print ("Введите название товара: ");
+        name = scn.next();
+        System.out.print("Введите цену товара: ");
+        price = scn.nextDouble();
+
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+          int guests;
+        while (true) {
+            System.out.print("Введите количество человек, на которых нужно разделить сумму чека: ");
+            guests = scn.nextInt();
+            if (guests > 1) {
+                // наверное, надо отсюда еще  класс Calculate запускать, если так можно
+                break;
+            } else if (guests < 1){
+                System.out.println("Количество человек введено не верно.");
+            }
+            else if (guests == 1) {
+                System.out.println("Один человек оплатит чек в полной сумме.");
+            }
+
+
+        };
+    }
+}
